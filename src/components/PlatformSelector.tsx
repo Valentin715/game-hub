d0ef4342371fb478d8fdbec2079f86be1fca53dd
Valentin715 <justvalk@yandex.ru>
2023-03-27 21:15:12 +0300

    Refactor: Extract a query object

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,22 +1,33 @@
-import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
+import { Menu, MenuButton, MenuItem, MenuList, Button } from "@chakra-ui/react";
+// import { Button } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import usePlatforms from "./../hooks/usePlatforms";
+import { Platform } from "./../hooks/useGames";
 
-const PlatformSelector = () => {
+interface Props {
+  onSelectPlatform: (plarform: Platform) => void;
+  selectedPlatfrom: Platform | null;
+}
+
+const PlatformSelector = ({ onSelectPlatform, selectedPlatfrom }: Props) => {
   const { data, error } = usePlatforms();
   if (error) return null;
 
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Platforms
-        <MenuList>
-          {data.map((platform) => (
-            <MenuItem key={platform.id}>{platform.name}</MenuItem>
-          ))}
-        </MenuList>
+        {selectedPlatfrom?.name || "Platforms"}
       </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
     </Menu>
   );
 };
